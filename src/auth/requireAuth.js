bc86d8ad3f8f13efe9db4c3c35f3a887180a3d23
Minhyeong Joe <minhyeong.billy.joe@gmail.com>
2026-05-08 15:15:22 -0700

    remove auth console log

--- src/auth/requireAuth.js
+++ src/auth/requireAuth.js
@@ -22,7 +22,7 @@ export async function requireAuth(req, res, next) {
 		const supabase = getSupabaseAuthClient();
 		const { data, error } = await supabase.auth.getUser(accessToken);
 		
-		console.log("Supabase auth response:", { data, error });
+		// console.log("Supabase auth response:", { data, error });
 		if (error || !data.user) {
 			return sendError(
 				res,
